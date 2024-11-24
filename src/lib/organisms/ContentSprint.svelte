ee7d807dd725f8fb4d1faf1d6a507181ec336493
Kaan <kaan.kalmioglu@hva.nl>
2024-11-24 16:05:15 +0100

    Removed unnecessary code

--- src/lib/organisms/ContentSprint.svelte
+++ src/lib/organisms/ContentSprint.svelte
@@ -5,7 +5,6 @@
   let { title, sprintNumber, content, startdate, topics } = $props();
 </script>
 
-<section>
   <article>
     <Heading title="Sprint {sprintNumber}: {title}" />
     <div>
@@ -15,25 +14,20 @@
     </div>
     <Topics {startdate} {topics} />
   </article>
-</section>
 
 <style>
-  section {
-    background-color: var(--grey);
-
-    article {
-      position: relative;
-      background-color: var(--white);
-      color: black;
-      width: fit-content;
-      border: 2px solid var(--turquoise);
-      border-radius: var(--rounded);
-      box-shadow: -4px 4px var(--lavender);
-      margin-left: var(--shadow);
-      min-height: 18rem;
-      max-width: 35em;
-      padding: 1.25em;
-    }
+  article {
+    position: relative;
+    background-color: var(--white);
+    color: black;
+    width: fit-content;
+    border: 2px solid var(--turquoise);
+    border-radius: var(--rounded);
+    box-shadow: -4px 4px var(--lavender);
+    margin-left: var(--shadow);
+    min-height: 18rem;
+    max-width: 35em;
+    padding: 1.25em;
   }
 
   article div { margin-top: 1.5rem; }
